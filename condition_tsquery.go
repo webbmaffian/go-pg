063f4e266fce11f84183980e0de83237bd57b8ff
Ivar Johansson <ivar@webbmaffian.se>
2022-12-05 11:00:48 +0100

    Added `Search()` and `PrefixSearch()`

---
+++ condition_tsquery.go
@@ -0,0 +1,61 @@
+package pg
+
+import "strings"
+
+var searchConfig string = "simple"
+var prefixSearchSanitizer = strings.NewReplacer(
+	`:`, `\:`,
+	`*`, `\*`,
+	`\`, `\\`,
+	`&`, `\&`,
+	`|`, `\|`,
+)
+
+func SetSearchConfig(conf string) {
+	searchConfig = conf
+}
+
+func PrefixSearch(vectorColumn any, value string) Condition {
+	words := strings.Split(value, " ")
+	terms := make([]string, 0, len(words))
+
+	for _, word := range words {
+		if word == "" {
+			continue
+		}
+
+		terms = append(terms, prefixSearchSanitizer.Replace(word))
+	}
+
+	return Search(vectorColumn, strings.Join(terms, " & "))
+}
+
+func Search(vectorColumn any, value any) Condition {
+	c := tsQuery{
+		value: value,
+	}
+
+	switch v := vectorColumn.(type) {
+	case Columnar:
+		c.column = v
+	case string:
+		c.column = Column(v)
+	}
+
+	return c
+}
+
+type tsQuery struct {
+	column Columnar
+	value  any
+}
+
+func (c tsQuery) encodeCondition(b *strings.Builder, args *[]any) {
+	c.column.encodeColumnIdentifier(b)
+
+	b.WriteString(" @@ to_tsquery('")
+	b.WriteString(searchConfig)
+	b.WriteString("', ")
+	writeParam(b, args, c.value)
+	b.WriteByte(')')
+}
