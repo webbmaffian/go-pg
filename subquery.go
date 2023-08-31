91318f43d77b6929b624abc9bc9e9822bf84586b
Ivar Johansson <ivar@webbmaffian.se>
2023-08-31 17:16:06 +0200

    Added support for selecting subquery column

--- subquery.go
+++ subquery.go
@@ -17,10 +17,70 @@ func (t SubquerySource) IsZero() bool {
 	return t.query.IsZero()
 }
 
+func (t SubquerySource) Column(path ...string) AliasedColumnar {
+	return subqueryColumn{
+		path:  path,
+		table: t.alias,
+	}
+}
+
 func (t SubquerySource) encodeQuery(b *strings.Builder, args *[]any) {
 	b.WriteByte('(')
 	t.query.encodeQuery(b, args)
 	b.WriteByte(')')
 	b.WriteString(" AS ")
 	writeIdentifier(b, t.alias)
+}
+
+type subqueryColumn struct {
+	path  []string
+	alias string
+	table string
+}
+
+func (c subqueryColumn) IsZero() bool {
+	return c.path == nil
+}
+
+func (c subqueryColumn) Alias(alias string) AliasedColumnar {
+	c.alias = alias
+	return c
+}
+
+func (c subqueryColumn) has(column string) bool {
+	return c.alias == column
+}
+
+func (c subqueryColumn) encodeColumn(b *strings.Builder) {
+	c.encode(b)
+
+	if c.alias != "" {
+		b.WriteString(" AS ")
+		writeIdentifier(b, c.alias)
+	}
+}
+
+func (c subqueryColumn) encodeColumnIdentifier(b *strings.Builder) {
+	if c.alias != "" {
+		writeIdentifier(b, c.alias)
+	} else {
+		c.encode(b)
+	}
+}
+
+func (c subqueryColumn) encode(b *strings.Builder) {
+	l := len(c.path)
+
+	if l > 1 {
+		b.WriteByte('(')
+
+		writeIdentifier(b, c.path[0])
+		b.WriteString(").")
+		c.path = c.path[1:]
+	} else if c.table != "" {
+		writeIdentifier(b, c.table)
+		b.WriteByte('.')
+	}
+
+	writeIdentifier(b, c.path...)
 }
